c6d767ec8a599d07ff873b2fb9c986f87e06e29e
zyqq <252545095@qq.com>
2019-03-29 20:25:14 +0800

    fix: 添加button-group中子元素标签的限制

--- src/button-group.vue
+++ src/button-group.vue
@@ -3,6 +3,19 @@
     <slot></slot>
   </div>
 </template>
+<script>
+export default {
+  mounted () {
+    for(let node of this.$el.children) {
+      let name = node.nodeName.toLowerCase()
+      if (name === 'div') {
+        console.warn(`w-button-group 的子元素必须是 w-button-group ，而你用的是 ${name}`)
+      }
+    }
+  }
+}
+</script>
+
 
 <style lang="scss">
   .w-button-group {
