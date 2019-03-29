0524f8722ad60acfdde32a4f84ffe2f857e84ce1
zyqq <252545095@qq.com>
2019-03-29 20:16:56 +0800

    feat: 添加button-group组件

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from './buttone.vue'
 import Icon from './icon.vue'
+import ButtonGroup from './button-group.vue'
 
 Vue.component('w-button', Button)
 Vue.component('w-icon', Icon)
+Vue.component('w-button-group', ButtonGroup)
 
 new Vue({
   el: '#app',
---
+++ src/button-group.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss">
+  .w-button-group {
+    display: inline-flex;
+    vertical-align: middle;
+  }
+</style>
+
