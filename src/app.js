b5e9da1123592e2a054ad321b6071517453f013a
zyqq <252545095@qq.com>
2019-03-29 23:23:15 +0800

    feat: 增加自动化测试以及持续集成

--- src/app.js
+++ src/app.js
@@ -16,78 +16,86 @@ new Vue({
   }
 })
 
-// 单元测试
-import chai from 'chai'
-import spies from 'chai-spies'
-chai.use(spies)
-const expect = chai.expect
-{
-  // 测试传入icon
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'setting'
-    }
-  })
-  vm.$mount()
-  let useElement = vm.$el.querySelector('use')
-  let href = useElement.getAttribute('xlink:href')
-  expect(href).to.eq('#i-setting')
-  vm.$el.remove()
-  vm.$destroy()
-}
-{
-  // 测试loading与icon同时传入
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'setting',
-      loading: true
-    }
-  })
-  vm.$mount()
-  let useElement = vm.$el.querySelector('use')
-  let href = useElement.getAttribute('xlink:href')
-  expect(href).to.eq('#i-loading')
-  vm.$el.remove()
-  vm.$destroy()
-}
-{
-  // 测试传入iconPosition
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'setting',
-      iconPosition: 'right'
-    }
-  })
-  // 挂载到具体实例上才能取到css样式
-  const div = document.createElement('div')
-  document.body.appendChild(div)
-  vm.$mount(div)
-  let useElement = vm.$el.querySelector('svg')
-  let { order } = window.getComputedStyle(useElement)
-  expect(order).to.eq("2")
-  // 销毁测试实例，防止内存泄露
-  vm.$el.remove()
-  vm.$destroy()
-}
-
-{
-  // 测试 click 事件
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'setting',
-    }
-  })
-  vm.$mount()
-  let spy = chai.spy(() => {})
-  vm.$on('click', spy)
-  // 希望 click 事件被触发
-  let button = vm.$el
-  button.click()
-  expect(spy).to.have.been.called()
-  vm.$el.remove()
-  vm.$destroy()
-}
+// // 单元测试
+// try {
+//   import chai from 'chai'
+//   import spies from 'chai-spies'
+//   chai.use(spies)
+//   const expect = chai.expect
+//   {
+//     // 测试传入icon
+//     const Constructor = Vue.extend(Button)
+//     const vm = new Constructor({
+//       propsData: {
+//         icon: 'setting'
+//       }
+//     })
+//     vm.$mount()
+//     let useElement = vm.$el.querySelector('use')
+//     let href = useElement.getAttribute('xlink:href')
+//     expect(href).to.eq('#i-setting')
+//     vm.$el.remove()
+//     vm.$destroy()
+//   }
+//   {
+//     // 测试loading与icon同时传入
+//     const Constructor = Vue.extend(Button)
+//     const vm = new Constructor({
+//       propsData: {
+//         icon: 'setting',
+//         loading: true
+//       }
+//     })
+//     vm.$mount()
+//     let useElement = vm.$el.querySelector('use')
+//     let href = useElement.getAttribute('xlink:href')
+//     expect(href).to.eq('#i-loading')
+//     vm.$el.remove()
+//     vm.$destroy()
+//   }
+//   {
+//     // 测试传入iconPosition
+//     const Constructor = Vue.extend(Button)
+//     const vm = new Constructor({
+//       propsData: {
+//         icon: 'setting',
+//         iconPosition: 'right'
+//       }
+//     })
+//     // 挂载到具体实例上才能取到css样式
+//     const div = document.createElement('div')
+//     document.body.appendChild(div)
+//     vm.$mount(div)
+//     let useElement = vm.$el.querySelector('svg')
+//     let { order } = window.getComputedStyle(useElement)
+//     expect(order).to.eq("2")
+//     // 销毁测试实例，防止内存泄露
+//     vm.$el.remove()
+//     vm.$destroy()
+//   }
+  
+//   {
+//     // 测试 click 事件
+//     const Constructor = Vue.extend(Button)
+//     const vm = new Constructor({
+//       propsData: {
+//         icon: 'setting',
+//       }
+//     })
+//     vm.$mount()
+//     let spy = chai.spy(() => {})
+//     vm.$on('click', spy)
+//     // 希望 click 事件被触发
+//     let button = vm.$el
+//     button.click()
+//     expect(spy).to.have.been.called()
+//     vm.$el.remove()
+//     vm.$destroy()
+//   }
+// } catch (error) {
+//   window.errors = [error]
+// } finally {
+//   window.errors && window.errors.forEach((error) => {
+//     console.error(error.message)
+//   })
+// }
--- src/button.vue
+++ src/button.vue
@@ -9,7 +9,11 @@
 </template>
 
 <script>
+import Icon from './icon'
 export default {
+  components: {
+    'w-icon': Icon
+  },
   props: {
     icon: {},
     loading: {
