732df91d7ca701a61f6c81543db5a6260f71ce04
zyqq <252545095@qq.com>
2019-03-29 20:21:16 +0800

    fix: 解决button-group 边界重叠问题

--- src/button-group.vue
+++ src/button-group.vue
@@ -8,6 +8,24 @@
   .w-button-group {
     display: inline-flex;
     vertical-align: middle;
+    > .w-button {
+      border-radius: 0;
+      &:not(:first-child) {
+        margin-left: -1px;
+      }
+      &:first-child {
+        border-top-left-radius: var(--border-radius);
+        border-bottom-left-radius: var(--border-radius);
+      }
+      &:last-child {
+        border-top-right-radius: var(--border-radius);
+        border-bottom-right-radius: var(--border-radius);
+      }
+      &:hover {
+        position: relative;
+        z-index: 1;
+      }
+    }
   }
 </style>
 
