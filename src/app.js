fb2af056e42e59f126bb7b2e2950388e98e101d0
zyqq <252545095@qq.com>
2019-03-29 14:48:11 +0800

    feat: 加入parcel,将button组件改为单文件组件

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import Button from './buttone.vue'
+
+Vue.component('g-button', Button)
+
+new Vue({
+  el: '#app',
+  data: {
+  }
+})
---
+++ src/buttone.vue
@@ -0,0 +1,32 @@
+<template>
+  <button class="g-button">按钮</button>  
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss">
+  .g-button {
+    height: var(--button-height);
+    padding: 0 1em;
+    font: inherit;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+    &:hover {
+      border-color: var(--border-color-hover);
+    }
+
+    &:active {
+      background: var(--button-active-bg);
+    }
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+</style>
