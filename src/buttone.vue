43f2e1ff65d4dc30fcc22076f706e0206432b791
zyqq <252545095@qq.com>
2019-03-29 19:26:40 +0800

    feat: 添加icon 以及iconPosition

--- src/buttone.vue
+++ src/buttone.vue
@@ -1,10 +1,26 @@
 <template>
-  <button class="g-button">按钮</button>  
+  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+    <svg v-if="icon" class="icon" aria-hidden="true">
+      <use :xlink:href="`#i-${icon}`" />
+    </svg>
+    <div class="content">
+      <slot />
+    </div>
+  </button>  
 </template>
 
 <script>
 export default {
-  
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validator (value) {
+        return value === 'left' || value === 'right'
+      }
+    }
+  }
 }
 </script>
 
@@ -12,20 +28,40 @@ export default {
   .g-button {
     height: var(--button-height);
     padding: 0 1em;
-    font: inherit;
+    font-size: var(--font-size);
     border-radius: var(--border-radius);
     border: 1px solid var(--border-color);
     background: var(--button-bg);
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
     &:hover {
       border-color: var(--border-color-hover);
     }
-
     &:active {
       background: var(--button-active-bg);
     }
 
     &:focus {
       outline: none;
+    }
+    > .icon {
+      order: 1;
+      margin-right: .1em;
+    }
+    > .content {
+      order: 2;
+    }
+    &.icon-right {
+      > .icon {
+        order: 2;
+        margin-right: 0;
+        margin-left: .1em;
+      }
+      > .content {
+        order: 1;
+      }
     }
   }
 
