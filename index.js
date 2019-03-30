3130d8e3880414d790920d5ab466426b9821a78c
zyqq <252545095@qq.com>
2019-03-30 17:46:31 +0800

    fix(package.json): 修这个版本与关键词

--- index.js
+++ index.js
@@ -2,15 +2,17 @@
  * @desc: { 文件入口-暴露组件给用户 } 
  * @author: zhengyiqiu 
  * @Create Date: 2019-03-30 00:03:24 
- * @Last Modified by: zhengyiqiu 
- * @Last Modified time: 2019-03-30 00:03:24 
+ * @Last Modified by: zhengyiqiu
+ * @Last Modified time: 2019-03-30 17:40:47
  */
 import Button from './src/button.vue'
 import ButtonGroup from './src/button-group.vue'
 import Icon from './src/icon.vue'
+import Input from './src/input.vue'
 
 export {
   Button,
   ButtonGroup,
-  Icon
+  Icon,
+  Input
 }
