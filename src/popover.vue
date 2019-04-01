0ab0286d17a6b47f55557d595b1ff66a6bb58b47
zyqq <252545095@qq.com>
2019-04-01 14:09:20 +0800

    feat(popover & tabs): popover增加弹出位置功能并且修复样式, tabs 重构

--- src/popover.vue
+++ src/popover.vue
@@ -1,53 +1,171 @@
 <template>
-  <div class="popover" @click.stop="showPopover">
-    <div class="content-wrapper" v-if="visible" @click.stop>
+  <div class="popover" @click="onClick" ref="popover">
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible"
+      :class="{[`position-${position}`]:true}">
       <slot name="content"></slot>
     </div>
-    <slot></slot>
+    <span ref="triggerWrapper" style="display: inline-block;">
+      <slot></slot>
+    </span>
   </div>
 </template>
+
 <script>
-export default {
-  name: 'WheelPopover',
-  data() {
-    return {
-      visible: false
-    }
-  },
-  methods: {
-    showPopover() {
-      this.visible = !this.visible
-      if (this.visible) {
+  export default {
+    name: "WheelPopover",
+    data () {
+      return {visible: false}
+    },
+    props: {
+      position: {
+        type: String,
+        default: 'top',
+        validator (value) {
+          return ['top', 'bottom', 'left', 'right'].indexOf(value) >= 0
+        }
+      }
+    },
+    methods: {
+      positionContent () {
+        const {contentWrapper, triggerWrapper} = this.$refs
+        document.body.appendChild(contentWrapper)
+        let {width, height, top, left} = triggerWrapper.getBoundingClientRect()
+        if (this.position === 'top') {
+          contentWrapper.style.left = left + window.scrollX + 'px'
+          contentWrapper.style.top = top + window.scrollY + 'px'
+        } else if (this.position === 'bottom') {
+          contentWrapper.style.left = left + window.scrollX + 'px'
+          contentWrapper.style.top = top + height + window.scrollY + 'px'
+        } else if (this.position === 'left') {
+          contentWrapper.style.left = left + window.scrollX + 'px'
+          let {height: height2} = contentWrapper.getBoundingClientRect()
+          contentWrapper.style.top = top + window.scrollY +
+            (height - height2) / 2 + 'px'
+        } else if (this.position === 'right') {
+          contentWrapper.style.left = left + window.scrollX + width + 'px'
+          let {height: height2} = contentWrapper.getBoundingClientRect()
+          contentWrapper.style.top = top + window.scrollY +
+            (height - height2) / 2 + 'px'
+        }
+      },
+      onClickDocument (e) {
+        if (this.$refs.popover &&
+          (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))
+        ) { return }
+        if (this.$refs.contentWrapper &&
+          (this.$refs.contentWrapper === e.target || this.$refs.contentWrapper.contains(e.target))
+        ) { return }
+        this.close()
+      },
+      open () {
+        this.visible = true
         this.$nextTick(() => {
-          // 需要给 click 事件命名，否则移除不了
-          // 但是不能直接在 removeEventListener 里用 function xxx().bind(this)，因为 bind 返回的是一个新函数，移除的就不是同一个函数了
-          let eventHandler = () => {
-            this.visible = false
-            console.log('document 隐藏 popover')
-            // popover 关闭后需要移除监听器，否则会累计添加监听器
-            document.removeEventListener('click', eventHandler)
-          }
-          // 得监听 document 不能监听 body，因为 body 高度可能没有覆盖整个页面
-          document.addEventListener('click', eventHandler)
+          this.positionContent()
+          document.addEventListener('click', this.onClickDocument)
         })
-      } else {
-          console.log('vm 隐藏 popover')
+      },
+      close () {
+        this.visible = false
+        document.removeEventListener('click', this.onClickDocument)
+      },
+      onClick (event) {
+        if (this.$refs.triggerWrapper.contains(event.target)) {
+          if (this.visible === true) {
+            this.close()
+            console.log('click close')
+          } else {
+            this.open()
+          }
+        }
       }
     }
   }
-}
 </script>
-<style lang="scss" scoped>
-.popover {
-  display: inline-block;
-  position: relative;
-  vertical-align: top;
+
+<style scoped lang="scss">
+  $border-color: #333;
+  $border-radius: 4px;
+  .popover {
+    display: inline-block;
+    vertical-align: top;
+    position: relative;
+  }
   .content-wrapper {
     position: absolute;
-    bottom: 100%;
-    left: 0;
-    border: 1px solid red;
-    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5)
+    border: 1px solid $border-color;
+    border-radius: $border-radius;
+    filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.5));
+    background: white;
+    padding: .5em 1em;
+    max-width: 20em;
+    word-break: break-all;
+    &::before, &::after {
+      content: '';
+      display: block;
+      border: 10px solid transparent;
+      width: 0;
+      height: 0;
+      position: absolute;
+    }
+    &.position-top {
+      transform: translateY(-100%);
+      margin-top: -10px;
+      &::before, &::after {
+        left: 10px;
+      }
+      &::before {
+        border-top-color: black;
+        top: 100%;
+      }
+      &::after {
+        border-top-color: white;
+        top: calc(100% - 1px);
+      }
+    }
+    &.position-bottom {
+      margin-top: 10px;
+      &::before, &::after {
+        left: 10px;
+      }
+      &::before {
+        border-bottom-color: black;
+        bottom: 100%;
+      }
+      &::after {
+        border-bottom-color: white;
+        bottom: calc(100% - 1px);
+      }
+    }
+    &.position-left {
+      transform: translateX(-100%);
+      margin-left: -10px;
+      &::before, &::after {
+        transform: translateY(-50%);
+        top: 50%;
+      }
+      &::before {
+        border-left-color: black;
+        left: 100%;
+      }
+      &::after {
+        border-left-color: white;
+        left: calc(100% - 1px);
+      }
+    }
+    &.position-right {
+      margin-left: 10px;
+      &::before, &::after {
+        transform: translateY(-50%);
+        top: 50%;
+      }
+      &::before {
+        border-right-color: black;
+        right: 100%;
+      }
+      &::after {
+        border-right-color: white;
+        right: calc(100% - 1px);
+      }
+    }
   }
-}
 </style>
--- src/tabs.vue
+++ src/tabs.vue
@@ -21,7 +21,7 @@ export default {
       }
     }
   },
-  data () {
+  data() {
     return {
       eventBus: new Vue()
     }
@@ -31,21 +31,29 @@ export default {
       eventBus: this.eventBus
     }
   },
-  mounted () {
-    if (this.$children.length === 0) {
-      console && console.warn &&
-      console.warn('tabs的子组件应该是tabs-head和tabs-nav，但你没有写子组件')
-    }
-    
-    this.$children.forEach((vm) => {
-      if (vm.$options.name === 'WheelTabsHead') {
-        vm.$children.forEach((childVm) => {
-          if(childVm.$options.name === 'WheelTabsItem' && childVm.name === this.selected) {
-            this.eventBus.$emit('update:selected', this.selected, childVm)
-          }
-        })
+  methods: {
+    checkChildren() {
+      if (this.$children.length === 0) {
+        console && console.warn &&
+          console.warn('tabs的子组件应该是tabs-head和tabs-nav，但你没有写子组件')
       }
-    })
+    },
+    selectTab() {
+      this.$children.forEach((vm) => {
+        if (vm.$options.name === 'WheelTabsHead') {
+          vm.$children.forEach((childVm) => {
+            if (childVm.$options.name === 'WheelTabsItem'
+              && childVm.name === this.selected) {
+              this.eventBus.$emit('update:selected', this.selected, childVm)
+            }
+          })
+        }
+      })
+    }
+  },
+  mounted() {
+    this.checkChildren()
+    this.selectTab()
   }
 }
 </script>
