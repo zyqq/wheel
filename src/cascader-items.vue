81ca453de7c961aa025bdda7615029eb75a531c2
zyqq <252545095@qq.com>
2019-04-16 00:08:55 +0800

    fix(cascader): 完善级联选择器样式

--- src/cascader-items.vue
+++ src/cascader-items.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="cascader-items">
+  <div class="cascader-items" :style="{height: height}">
     <div class="left">
       <div class="label" v-for="(item, index) in items" :key="index" @click="leftSelected=item">
         {{item.name}}
+        <icon class="icon" name="right" v-if="item.children"></icon>
       </div>
     </div>
     <div class="right" v-if="rightItems">
@@ -11,11 +12,18 @@
   </div>
 </template>
 <script>
+import Icon from './icon'
 export default {
   name: 'WheelCascaderItems',
+  components: {
+    Icon
+  },
   props: {
     items: {
       type: Array
+    },
+    height: {
+      type: String
     }
   },
   data() {
@@ -25,7 +33,7 @@ export default {
   },
   computed: {
     rightItems() {
-      if(this.leftSelected && this.leftSelected.children) {
+      if (this.leftSelected && this.leftSelected.children) {
         return this.leftSelected.children
       } else {
         return null
@@ -35,16 +43,28 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-  @import 'var';
-  .cascader-items {
+@import "var";
+.cascader-items {
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+  height: 100%;
+  .left {
+    height: 100%;
+    padding: 0.3em 0;
+  }
+  .right {
+    height: 100%;
+    border-left: 1px solid $border-color-light;
+  }
+  .label {
+    padding: 0.3em 1em;
     display: flex;
-    align-items: flex-start;
-    justify-content: flex-start;
-    .left {
-      border: 1px solid red;
-    }
-    .right {
-      margin-top: -1px;
+    align-items: center;
+    .icon {
+      margin-left: 1em;
+      transform: scale(0.5);
     }
   }
+}
 </style>
--- src/cascader.vue
+++ src/cascader.vue
@@ -3,8 +3,8 @@
     <div class="trigger" @click="popoverVisible = !popoverVisible">
       <slot></slot>
     </div>
-    <div class="popover" v-if="popoverVisible">
-      <cascader-items :items="source"></cascader-items>
+    <div class="popover-wrapper" v-if="popoverVisible">
+      <cascader-items class="popover" :items="source" :height="popoverHeight"></cascader-items>
     </div>
   </div>
 </template>
@@ -16,6 +16,9 @@ export default {
   props: {
     source: {
       type: Array
+    },
+    popoverHeight: {
+      type: String
     }
   },
   data() {
@@ -28,15 +31,19 @@ export default {
 <style lang="scss" scoped>
   @import 'var';
   .cascader {
+    position: relative;
     .trigger {
-      border: 1px solid red;
+      border: 1px solid black;
       height: 32px;
       width: 100px;
     }
-    .popover {
-      border: 2px solid green;
-      height: 200px;
+    .popover-wrapper {
+      position: absolute; 
       display: flex;
+      top: 100%; 
+      left: 0; 
+      @extend .box-shadow;
+      background: white; 
     }
   }
 </style>
