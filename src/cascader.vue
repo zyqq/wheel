f0ee2ff4d6840c83aabd362668dae5c4f882c7bd
zyqq <252545095@qq.com>
2019-05-21 15:28:46 +0800

    fix(cascader): 完善基本样式

--- src/cascader.vue
+++ src/cascader.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="cascader">
     <div class="trigger" @click="popoverVisible = !popoverVisible">
-      <slot></slot>
+      {{result || '&nbsp;'}}
     </div>
     <div class="popover-wrapper" v-if="popoverVisible">
       <cascader-items class="popover" :items="source" :height="popoverHeight" :selected="selected" @update:selected="onUpdateSelected"></cascader-items>
@@ -30,6 +30,11 @@ export default {
       popoverVisible: false
     }
   },
+  computed: {
+    result() {
+      return this.selected.map(item => item.name).join('/')
+    }
+  },
   methods: {
     onUpdateSelected(newSelected) {
       this.$emit('update:selected', newSelected)
@@ -42,15 +47,20 @@ export default {
   .cascader {
     position: relative;
     .trigger {
-      border: 1px solid black;
-      height: 32px;
-      width: 100px;
+      height: $input-height;
+      display: inline-flex;
+      align-items: center;
+      padding: 0 1em;
+      min-width: 10em;
+      border: 1px solid $border-color;
+      border-radius: $border-radius;
     }
     .popover-wrapper {
       position: absolute; 
       display: flex;
       top: 100%; 
       left: 0; 
+      margin-top: 8px;
       @extend .box-shadow;
       background: white; 
     }
