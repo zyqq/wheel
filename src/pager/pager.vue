1194f6a7ad6b4331ab8813c74880c5ee95cb2b01
zyqq <252545095@qq.com>
2019-05-31 18:44:55 +0800

    fix(pager): 完成大部分样式

--- src/pager/pager.vue
+++ src/pager/pager.vue
@@ -1,14 +1,30 @@
 <template>
   <div class="wheel-pager">
-    <span v-for="page in pages" :key="page" class="wheel-pager-item" 
-    :class="{active: page===currentPage, separator: page === '...'}">
-    {{page}}
+    <span class="wheel-pager-nav prev" :class="{disabled:currentPage===1}">
+      <w-icon name="left"></w-icon>
+    </span>
+    <template v-for="page in pages">
+      <template v-if="page === currentPage">
+        <span :key="page" class="wheel-pager-item current">{{page}}</span>
+      </template>
+      <template v-else-if="page === '...'">
+        <w-icon :key="page" class="wheel-pager-separator" name="dots"></w-icon>
+      </template>
+      <template v-else>
+        <span :key="page" class="wheel-pager-item other">{{page}}</span>
+      </template>
+    </template>
+    <span class="wheel-pager-nav next" :class="{disabled: currentPage===totalPage}">
+      <w-icon name="right"></w-icon>
     </span>
   </div>
 </template>
 <script>
+import WIcon from '../icon/icon'
+
 export default {
   name: 'WheelPager',
+  components: {WIcon},
   props: {
     currentPage: {
       type: Number,
@@ -26,10 +42,11 @@ export default {
   data() {
     let {currentPage, totalPage} = this
     let pages = unique([1, currentPage,currentPage-1, currentPage-2, currentPage +1, currentPage +1, totalPage])
+                .filter((n) => n >= 1 && n <= totalPage)
                 .sort((a, b) => a-b)
                 .reduce((prev, current, index, array) => {
                   prev.push(current)
-                  array[index+1] && array[index+1] - array[index-1] > 1 && array.push('...')
+                  array[index+1] && array[index+1] - array[index] > 1 && prev.push('...')
                   return prev
                 }, [])
     return {
@@ -47,18 +64,27 @@ function unique(array) {
 </script>
 <style scoped lang="scss">
   @import "../../styles/_var.scss";
-  .wheel-pager {
+  .wheel-pager { 
+    display: flex; justify-content: flex-start; align-items: center;
+    $width: 20px;
+    $height: 20px;
+    $font-size: 12px;
+    &-separator {
+      width: $width;
+      font-size: $font-size;
+    }
     &-item {
+      min-width: $width; height: $height;font-size: $font-size;
       border: 1px solid #e1e1e1; border-radius: $border-radius; padding: 0 4px; display: inline-flex; justify-content: center;
-      align-items: center; font-size: 12px; min-width: 20px; height: 20px; margin: 0 4px; cursor: pointer;
-      &.separator {
-        border: none;
-      }
-      &.active, &:hover {
-        border-color: $blue;
-      }
-      &.active {
-        cursor: default;
+      align-items: center; margin: 0 4px; cursor: pointer;
+      &.current, &:hover { border-color: $blue; }
+      &.current { cursor: default; }
+    }
+    &-nav {
+      margin: 0 4px; display: inline-flex; justify-content: center; align-items: center;
+      background: $grey; height: $height; width: $width; border-radius: $border-radius; font-size: $font-size;
+      &.disabled {
+        svg { fill: darken($grey, 30%); }
       }
     }
   }
