32d1e67f7819d3c1ed3bfb57f3179390a26ff889
zyqq <252545095@qq.com>
2019-06-01 16:33:54 +0800

    feat(table): table组件基本架构完成

--- src/demo.vue
+++ src/demo.vue
@@ -1,19 +1,45 @@
 <template>
-  <div style="margin: 20px;">
-    <w-pager :total-page="10" :current-page.sync="currentPage"></w-pager>
+  <div>
+    {{selected}}
+    <div style="margin: 20px;">
+      <w-table :columns="columns" :data-source="dataSource" bordered :selected-items.sync="selected"></w-table>
+    </div>
+    <div style="margin: 20px;">
+      <w-table :columns="columns" :data-source="dataSource" bordered compact :striped="false"></w-table>
+    </div>
+    <div style="margin: 20px;">
+      <w-pager :total-page="10" :current-page.sync="currentPage"></w-pager>
+    </div>
   </div>
 </template>
 <script>
   import WPager from './pager/pager'
+  import WTable from './table/table'
 
   export default {
     name: "demo",
     components: {
-      WPager
+      WPager,
+      WTable
     },
     data() {
       return {
-        currentPage: 1
+        currentPage: 1,
+        selected: [],
+        columns: [
+          {text: '姓名', field: 'name'},
+          {text: '分数', field: 'score'},
+        ],
+        dataSource: [
+          {id: 1, name: '方方', score: 100},
+          {id: 2, name: '圆圆', score: 99},
+          {id: 3, name: '张三', score: 100},
+          {id: 4, name: '李四', score: 99},
+          {id: 5, name: '超人', score: 100},
+          {id: 6, name: '蝙蝠侠', score: 99},
+          {id: 7, name: '蜘蛛侠', score: 100},
+          {id: 8, name: '钢铁侠', score: 99},
+        ]
       }
     },
   };
---
+++ src/table/table.vue
@@ -0,0 +1,135 @@
+<template>
+  <div class="wheel-table-wrapper">
+    <table class="wheel-table" :class="{bordered, compact, striped: striped}">
+      <thead>
+      <tr>
+        <th><input type="checkbox" @change="onChangeAllItems" ref="allChecked"/></th>
+        <th v-if="numberVisible">#</th>
+        <th v-for="column in columns" :key="column.field">
+          {{column.text}}
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(item,index) in dataSource" :key="item.id">
+        <td>
+          <input type="checkbox" @change="onChangeItem(item, index, $event)"
+            :checked="inSelectedItems(item)"
+          /></td>
+        <td v-if="numberVisible">{{index+1}}</td>
+        <template v-for="column in columns">
+          <td :key="column.field">{{item[column.field]}}</td>
+        </template>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "WheelTable",
+    props: {
+      striped: {
+        type: Boolean,
+        default: true
+      },
+      selectedItems: {
+        type: Array,
+        default: () => []
+      },
+      compact: {
+        type: Boolean,
+        default: false
+      },
+      columns: {
+        type: Array,
+        required: true
+      },
+      dataSource: {
+        type: Array,
+        required: true,
+        validator (array) {
+          return !(array.filter(item => item.id === undefined).length > 0)
+        }
+      },
+      numberVisible: {
+        type: Boolean,
+        default: false
+      },
+      bordered: {
+        type: Boolean,
+        default: false
+      }
+    },
+    watch: {
+      selectedItems () {
+        if (this.selectedItems.length === this.dataSource.length) {
+          this.$refs.allChecked.indeterminate = false
+        } else if (this.selectedItems.length === 0) {
+          this.$refs.allChecked.indeterminate = false
+        } else {
+          this.$refs.allChecked.indeterminate = true
+        }
+      }
+    },
+    methods: {
+      inSelectedItems (item) {
+        return this.selectedItems.filter(i => i.id === item.id).length > 0
+      },
+      onChangeItem (item, index, e) {
+        let selected = e.target.checked
+        let copy = JSON.parse(JSON.stringify(this.selectedItems))
+        if (selected) {
+          copy.push(item)
+        } else {
+          copy = copy.filter(i => i.id !== item.id)
+        }
+        this.$emit('update:selectedItems', copy)
+      },
+      onChangeAllItems (e) {
+        let selected = e.target.checked
+        this.$emit('update:selectedItems', selected ? this.dataSource : [])
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  @import "../../styles/_var.scss";
+  $grey: darken($grey, 10%);
+  .wheel-table {
+    width: 100%;
+    border-collapse: collapse;
+    border-spacing: 0;
+    border-bottom: 1px solid $grey;
+    &.bordered {
+      border: 1px solid $grey;
+      td, th {
+        border: 1px solid $grey;
+      }
+    }
+    &.compact {
+      td, th {
+        padding: 4px;
+      }
+    }
+    td, th {
+      border-bottom: 1px solid $grey;
+      text-align: left;
+      padding: 8px;
+    }
+    &.striped {
+      tbody {
+        > tr {
+          &:nth-child(odd) {
+            background: white;
+          }
+          &:nth-child(even) {
+            background: lighten($grey, 10%);
+          }
+        }
+      }
+    }
+  }
+</style> 
